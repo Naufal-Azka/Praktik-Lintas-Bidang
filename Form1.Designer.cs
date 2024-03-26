610390713f47d85d6fe9f2fbcc59d1fc294bde09
AkuSinyiting <askanaufal74@gmail.com>
2024-03-27 03:28:14 +0700

    Reapply "Add project files."
    This reverts commit f3fd2db86a7174ecad1c7be41865ddb9942b5e40.

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace PLB
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panelVitamin = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelObat = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panelSuplemen = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panelVitamin.SuspendLayout();
+            this.panelObat.SuspendLayout();
+            this.panelSuplemen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1370, 120);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Cascadia Mono", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(102, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 100);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Hudava";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.homeClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::PLB.Properties.Resources.Logo_PLB;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.homeClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(15, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.obatClick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.ErrorImage = null;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.InitialImage = null;
+            this.pictureBox3.Location = new System.Drawing.Point(18, 13);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.vitaminClick);
+            // 
+            // panelVitamin
+            // 
+            this.panelVitamin.BackColor = System.Drawing.Color.White;
+            this.panelVitamin.Controls.Add(this.panel3);
+            this.panelVitamin.Controls.Add(this.label2);
+            this.panelVitamin.Controls.Add(this.pictureBox3);
+            this.panelVitamin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelVitamin.Location = new System.Drawing.Point(45, 176);
+            this.panelVitamin.Name = "panelVitamin";
+            this.panelVitamin.Size = new System.Drawing.Size(400, 220);
+            this.panelVitamin.TabIndex = 7;
+            this.panelVitamin.Click += new System.EventHandler(this.vitaminClick);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel3.Location = new System.Drawing.Point(0, 170);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(400, 50);
+            this.panel3.TabIndex = 8;
+            this.panel3.Click += new System.EventHandler(this.vitaminClick);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Cascadia Mono", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(174, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 150);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Vitamin";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.vitaminClick);
+            // 
+            // panelObat
+            // 
+            this.panelObat.BackColor = System.Drawing.Color.White;
+            this.panelObat.Controls.Add(this.panel5);
+            this.panelObat.Controls.Add(this.label3);
+            this.panelObat.Controls.Add(this.pictureBox2);
+            this.panelObat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelObat.Location = new System.Drawing.Point(490, 176);
+            this.panelObat.Name = "panelObat";
+            this.panelObat.Size = new System.Drawing.Size(400, 220);
+            this.panelObat.TabIndex = 9;
+            this.panelObat.Click += new System.EventHandler(this.obatClick);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel5.Location = new System.Drawing.Point(0, 170);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(400, 50);
+            this.panel5.TabIndex = 9;
+            this.panel5.Click += new System.EventHandler(this.obatClick);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(171, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(226, 150);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Obat";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.obatClick);
+            // 
+            // panelSuplemen
+            // 
+            this.panelSuplemen.BackColor = System.Drawing.Color.White;
+            this.panelSuplemen.Controls.Add(this.panel7);
+            this.panelSuplemen.Controls.Add(this.label4);
+            this.panelSuplemen.Controls.Add(this.pictureBox4);
+            this.panelSuplemen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelSuplemen.Location = new System.Drawing.Point(937, 176);
+            this.panelSuplemen.Name = "panelSuplemen";
+            this.panelSuplemen.Size = new System.Drawing.Size(400, 220);
+            this.panelSuplemen.TabIndex = 10;
+            this.panelSuplemen.Click += new System.EventHandler(this.suplemenClick);
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel7.Location = new System.Drawing.Point(0, 170);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(400, 50);
+            this.panel7.TabIndex = 0;
+            this.panel7.Click += new System.EventHandler(this.suplemenClick);
+            // 
+            // label4
+            // 
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(173, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 150);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Suplemen";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.suplemenClick);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(16, 13);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.suplemenClick);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1370, 692);
+            this.Controls.Add(this.panelVitamin);
+            this.Controls.Add(this.panelSuplemen);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelObat);
+            this.Font = new System.Drawing.Font("Cascadia Mono", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hudava";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panelVitamin.ResumeLayout(false);
+            this.panelObat.ResumeLayout(false);
+            this.panelSuplemen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panelVitamin;
+        private System.Windows.Forms.Panel panelObat;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panelSuplemen;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
